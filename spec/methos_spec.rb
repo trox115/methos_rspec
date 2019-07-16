7d3718e0802037acda75acd49ced47ce576fc4fb
trox115 <tofernandes67@gmail.com>
2019-07-17 00:49:29 +0100

    my each with rspec

---
+++ spec/methos_spec.rb
@@ -0,0 +1,9 @@
+require '../methods.rb'
+RSpec.describe Enumerable do
+    describe "#my_each" do
+        it "Takes a block as argument" do
+            expect([1,2,3].my_each {|x| x}).to eql([1,2,3])
+        end
+    end
+   
+end
