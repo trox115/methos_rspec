05fd933cc478e3f5752b71f8004dac50421ddfd1
trox115 <tofernandes67@gmail.com>
2019-07-17 00:52:39 +0100

    added feature my all

--- spec/methos_spec.rb
+++ spec/methos_spec.rb
@@ -5,5 +5,13 @@ RSpec.describe Enumerable do
             expect([1,2,3].my_each {|x| x}).to eql([1,2,3])
         end
     end
-   
+    describe '#my_select' do
+        it "Returns all even numbers." do
+            expect([1,2,3,4,5].my_select { |num|  num.even?  }).to eql([2,4])
+        end
+        it "returns are element vogals." do
+            a = %w{ a b c d e f }
+            expect(a.my_select { |v| v =~ /[aeiou]/ }).to eql(["a", "e"])
+        end
+    end
 end
